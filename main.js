337f04a24504200581eea77a55cb7acd4d4f41ed
Ian <ian@beeswax.com>
2018-07-06 10:28:36 -0400

    edits

--- main.js
+++ main.js
@@ -1,24 +1,43 @@
 var bidInput = $('#bid-input');
-var regex = /^\s*?\d+(\:.*\n|\s*?\{[\s\w\W]*?\}\n)/gm;
+var regex = /^\s*?\d+(\:\s?.*\n?|\s*?\{[\s\w\W]*?\}\n?)/gm;
+$('#submit').on('click', submit);
 
-function submit() {
+function submit(e) {
   var rawBidRequest = bidInput[0].value;
-  var output = rawBidRequest.replace(regex, "");
-  var pre = $("pre");
+  var output = rawBidRequest.replace(regex, function(match) {
+    return "<span class='deletion'>"+match+"</span>";
+  });
+  var newProto = rawBidRequest.replace(regex, "");
+  $("pre").html("");
+  $("pre").append(newProto);
+  var container = $(".output");
   var label = $("#output-label");
-  if (pre.length > 0) { $(pre).remove(); }
+  if (container.length > 0) { $(container).remove(); }
   if (label) { $(label).remove(); }
   $("body").append(
-    "<label id='output-label'>Output:&emsp;"
-    + "<i class='fa fa-copy' onclick='copy()'></i>&emsp;"
-    + "<i class='fa fa-download'></label>"
+    "<label id='output-label'>Output:&ensp;"
+    + "<i class='fa fa-copy copy' id='copy'></i>&nbsp<i style='font-size:12px'>"
+    + "(deletions highlighted in <span class='deletion'>red </span>)</i>"
+    + "</label>"
   );
-  $("body").append($(`<pre>${output}</pre>`));
+  $("body").append($("<div class='output'>"+output+"</div>"));
+  $("#copy").on('click', copy);
 }
 
-function copy() {
+function copy(e) {
   var pre = $("pre");
   window.getSelection().selectAllChildren(pre[0]);
   document.execCommand("copy");
   window.getSelection().empty();
+  showToolTip(e);
+}
+
+function showToolTip(e) {
+  var x = e.clientX - 20 + "px",
+    y = e.clientY - 50 + "px",
+    toolTip = $('<div class="copied-tooltip">Copied to Clipboard</div>')
+    .css("top", y).css("left", x);
+  $(document.body).append(toolTip)
+  setTimeout(function(){ $(".copied-tooltip").css("opacity", 0); }, 750);
+  setTimeout(function(){ $(".copied-tooltip").remove(); }, 1250);
 }
